ab5f76b0fd62632c5daaeace64ec1b4ee397db91
Pedro Rodrigues <pedroras004@gmail.com>
2020-12-28 22:32:52 -0300

    AULAS1-3

---
+++ aula03/Criar tabela.sql
@@ -0,0 +1,11 @@
+use cadastro;
+CREATE TABLE pessoas (
+    nome VARCHAR(30),
+    idade TINYINT,
+    sexo CHAR(1),
+    peso FLOAT,
+    altura FLOAT,
+    nacionalidade VARCHAR(20)
+);
+
+describe pessoas;
